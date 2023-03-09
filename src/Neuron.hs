d7e0d5a0dd753f6e8b91ccc642e268c0bd8a21fc
jacksonneal <jacksontneal@gmail.com>
2023-03-08 23:31:50 -0500

    organize

--- src/Neuron.hs
+++ src/Neuron.hs
@@ -1,9 +1,12 @@
 module Neuron where
 
+-- linear algebra
+
 dot :: Num a => [a] -> [a] -> a
 dot = (sum .) . zipWith (*)
 
 t :: [[a]] -> [[a]]
+t ([] : _) = []
 t x = map head x : t (map tail x)
 
 vm :: Num a => [a] -> [[a]] -> [a]
@@ -15,19 +18,29 @@ mv = flip (map . flip dot)
 mm :: Num a => [[a]] -> [[a]] -> [[a]]
 mm = (. t) . map . mv
 
-perceptron :: (Ord a, Num a) => a -> Int
+-- activation
+
+type Activation = Float -> Float
+
+perceptron :: Activation
 perceptron x
   | x <= 0 = 0
   | otherwise = 1
 
-sigmoid :: Float -> Float
+sigmoid :: Activation
 sigmoid z = 1 / (1 + exp (-z))
 
-ffl :: [[Float]] -> [[Float]] -> [Float] -> [[Float]]
-ffl xss wss bs = map (\xs -> zipWith (sigmoid xs) wss bs) xss
+-- util
 
-ffn :: [[Float]] -> [[[Float]]] -> [[Float]] -> [[Float]]
-ffn xss wsss bss = foldr (\(wss, bs) xss -> xss) xss (zip wsss bss)
+fmapNested :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
+fmapNested = fmap . fmap
+
+-- layers
+
+fcl :: [[Float]] -> [[Float]] -> Activation -> [[Float]]
+fcl = (flip fmapNested .) . mm
+
+-- cost functions
 
 mse :: [Float] -> [Float] -> Float
 mse ys as = (1 / (2 * n)) * sse
