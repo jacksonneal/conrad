73f00793186d74f2223fb6a6558f0b2440765ea4
jacksonneal <jacksontneal@gmail.com>
2023-04-15 18:31:09 -0400

    work on tape strategy

--- src/Prelude/Conrad.hs
+++ src/Prelude/Conrad.hs
@@ -90,52 +90,35 @@ fn3 [x, y] = [x * x * y, x + y]
 fn3' :: Floating a => [a] -> [[a]]
 fn3' = jacobianF fn3
 
-data Expr = Var Int | Const Double | Add Expr Expr | Mul Expr Expr | Sin Expr
+data Tape a = Tape {value :: a, tangents :: Map.Map (Tape a) a} deriving (Eq)
 
-data Tape = Tape {output :: Double, varTapes :: Map.Map Int Tape} deriving (Show)
+instance Ord a => Ord (Tape a) where
+  compare (Tape v1 _) (Tape v2 _) = compare v1 v2
 
-eval :: Map.Map Int Double -> Expr -> Double
-eval env (Var x) = case Map.lookup x env of
-  Just v -> v
-  Nothing -> error $ "Variable not found: " ++ show x
-eval _ (Const x) = x
-eval env (Add e1 e2) = eval env e1 + eval env e2
-eval env (Mul e1 e2) = eval env e1 * eval env e2
-eval env (Sin e) = sin (eval env e)
+instance (Num a, Ord a) => Num (Tape a) where
+  x@(Tape vx _) + y@(Tape vy _) = Tape (vx + vy) (Map.fromList [(x, 1), (y, 1)])
+  x@(Tape vx _) - y@(Tape vy _) = Tape (vx - vy) (Map.fromList [(x, -1), (y, -1)])
+  x@(Tape vx _) * y@(Tape vy _) = Tape (vy * vy) (Map.fromList [(x, vy), (y, vx)])
+  abs x@(Tape v _) = Tape (abs v) (Map.singleton x (signum v))
+  signum x@(Tape v _) = Tape (signum v) (Map.singleton x 0)
+  fromInteger x = Tape (fromInteger x) Map.empty
 
-reverseDiff :: Expr -> Double -> (Tape, Double)
-reverseDiff f x = (t, getDeriv t)
-  where
-    t = evalTape (Tape (eval (Map.singleton 1 x) f) empty) f
-    empty = Map.empty
+instance (Fractional a, Ord a) => Fractional (Tape a) where
+  x@(Tape vx _) / y@(Tape vy _) = Tape (vx / vy) (Map.fromList [(x, 1 / vy), (y, -vx / vy ^ 2)])
+  fromRational x = Tape (fromRational x) Map.empty
 
-evalTape :: Tape -> Expr -> Tape
-evalTape t (Var varId) = case Map.lookup varId (varTapes t) of
-  Just t' -> t'
-  Nothing -> error $ "Unbound variable " ++ show varId
-evalTape t (Const c) = t
-evalTape t (Add e1 e2) =
-  let t1 = evalTape t e1
-      t2 = evalTape t e2
-      x1 = output t1
-      x2 = output t2
-   in t2 {output = x1 + x2}
-evalTape t (Mul e1 e2) =
-  let t1 = evalTape t e1
-      t2 = evalTape t e2
-      x1 = output t1
-      x2 = output t2
-   in t2 {output = x1 * x2}
-evalTape t (Sin e) =
-  let t' = evalTape t e
-      x = output t'
-      y = cos x
-   in t' {output = sin x, varTapes = Map.map (scaleTape y) (varTapes t')}
-
-getDeriv :: Tape -> Double
-getDeriv t = case Map.elems (varTapes t) of
-  [t'] -> output (evalTape t' (Const 1.0))
-  _ -> error "Multiple variables found in tape"
-
-scaleTape :: Double -> Tape -> Tape
-scaleTape s t = t {output = s * output t, varTapes = Map.map (scaleTape s) (varTapes t)}
+instance (Floating a, Ord a) => Floating (Tape a) where
+  pi = Tape pi Map.empty
+  exp x@(Tape v _) = Tape (exp v) (Map.fromList [(x, exp v)])
+  log x@(Tape v _) = Tape (log v) (Map.fromList [(x, 1 / v)])
+  sin x@(Tape v _) = Tape (sin v) (Map.fromList [(x, cos v)])
+  cos (Dual x x') = Dual (cos x) (-x' * sin x)
+  asin (Dual x x') = Dual (asin x) (x' / sqrt (1 - x * x))
+  acos (Dual x x') = Dual (acos x) (-x' / sqrt (1 - x * x))
+  atan (Dual x x') = Dual (atan x) (x' / (1 + x * x))
+  sinh (Dual x x') = Dual (sinh x) (x' * cosh x)
+  cosh (Dual x x') = Dual (cosh x) (x' * sinh x)
+  tanh (Dual x x') = Dual (tanh x) (x' / (cosh x * cosh x))
+  asinh (Dual x x') = Dual (asinh x) (x' / sqrt (1 + x * x))
+  acosh (Dual x x') = Dual (acosh x) (x' / sqrt (x * x - 1))
+  atanh (Dual x x') = Dual (atanh x) (x' / (1 - x * x))
